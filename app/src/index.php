38d46fedd115efa0aeb70319953e2c2d277cf445
luansena@live.com <luansena@live.com>
2016-11-27 02:21:57 -0200

    config

--- app/src/index.php
+++ app/src/index.php
@@ -34,30 +34,30 @@ $list = [
     ["lat" => -22.98261443, "lon" => -43.36488247],
 	["lat" => -23.0080965, "lon" => -43.3043155]
 ];
- 
+
 $app->get('/', function() use ($list, $app) {
 	return "Hello Word!";
 });
 
-$app->match('/v1/sign-up', function (Request $request) use ($app) {
+$app->post('/v1/sign-up', function (Request $request) use ($app) {
     var_dump('aaaa');exit;
-    $content = json_decode($request->getContent());
-    var_dump(array(
-            'nome' => $content->nome,
-            'email' => $content->email,
-            'senha' => $content->senha
-        ));exit;
-
-	$post = $app['db']->insert('usuario',
-        array(
-            'nome' => $content->nome,
-            'email' => $content->email,
-            'senha' => $content->senha
-        )
-    );
+//    $content = json_decode($request->getContent());
+//    var_dump(array(
+//            'nome' => $content->nome,
+//            'email' => $content->email,
+//            'senha' => $content->senha
+//        ));exit;
+//
+//	$post = $app['db']->insert('usuario',
+//        array(
+//            'nome' => $content->nome,
+//            'email' => $content->email,
+//            'senha' => $content->senha
+//        )
+//    );
 
 	return '';
-})->method('OPTIONS|POST');
+});
 
 $app->post('/v1/questions', function (Request $request) {
     $content = json_decode($request->getContent());
@@ -74,5 +74,5 @@ $app->get('/v1/risk-area', function() use ($list) {
 $app->get('/v1/ccurrences', function() use ($list) {
 	return json_encode($list);
 });
- 
+
 $app->run();
